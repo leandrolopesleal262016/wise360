c7b709d6ff1af9331a700c26c78ca57f103245bb
leandrolopesleal26 <leandrolopesleal26@gmail.com>
2024-12-06 15:37:15 -0300

    Primeiro commit

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function Card({ className, ...props }: CardProps) {
+  return (
+    <div
+      className={`rounded-lg border bg-card text-card-foreground shadow-sm ${className}`}
+      {...props}
+    />
+  )
+}
+Card.displayName = "Card"
